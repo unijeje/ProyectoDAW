cae8a3feb28cda505f3029414812961e1fbd04ad
unijeje <veturn@hotmail.com>
2018-03-24 15:48:31 +0100

    alta juego y alta en company_juegos

---
+++ js/addJuego.js
@@ -0,0 +1,153 @@
+﻿var ids=[];
+$( function() {
+        var today = new Date();
+        var maxYear=today.getFullYear()+3;
+        $("#fecha").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            showButtonPanel: true,
+            dateFormat: 'yy-mm-dd',
+            yearRange: "1950:"+maxYear
+        });
+} );
+
+
+$("#btnADD").click(addJuego);
+$("#btnCompany").click(addCompany);
+$("#registrarOtro").click(mostrarForm);
+
+$.get("../servidor/gestionCompany/autoCompleteCompany.php", respuestaAutoCompleteCompany, "json");
+
+function addCompany()
+{
+    var sHtml='<div style="display:none;" class="form-group divCompany"><label for="company">Compañía: *</label>';
+    sHtml+='<div class="form-inline">';
+    sHtml+='<input type="text" class="form-control col-6 companies" placeholder="Nombre completo" name="company">';
+    sHtml+='<input type="button" class="btn btn-danger col-1 ml-1 btnEliminar" value="X" />';
+    sHtml+='</div>';
+    sHtml+='</div>';
+    $("#btnCompany").parent().before(sHtml);
+    $(".divCompany:last").show("slow");
+
+    var botonesEliminar=$(".btnEliminar:last");
+    var camposCompanies=$(".companies:last");
+    $(botonesEliminar).click(ocultarCompany);
+    $(camposCompanies).autocomplete({
+        source: ids,
+        minLength: 0,
+        select: function(event, ui){
+            $(camposCompanies).val(ui.item.value);
+             //$("#cliente-dni").val(ui.item.value);
+             return false;
+        }}).autocomplete("instance")._renderItem=function(ul, item){
+            return $("<li>").append("<div>"+item.value+"<br>"+item.desc+"</div>").appendTo(ul);
+        };
+
+}
+
+function ocultarCompany(event)
+{
+    $( event.target ).parent().parent().hide('slow', function(){ $target.remove(); });
+
+}
+
+function mostrarForm()
+{
+    $("#formAddJuego").show();
+    $("#guidelines").show();
+    $("h1").show();
+    $("#registroError").hide();
+    $("#registrarOtro").hide();
+    $("#registrado").hide();
+}
+
+function addJuego()
+{
+    $("#guidelines").hide();
+    $("#registroError").hide();
+    $("#registrarOtro").hide();
+    if(validarJuego())
+    {
+        var botonesEliminar=$(".btnEliminar");
+        var sNombre=$("#nombre").val().trim();
+        var sSinopsis=$("#sinopsis").val().trim();
+        var sEnlace=$("#enlace").val().trim();
+        var sFecha=$('#fecha').val().trim();
+        var valoresCompany=[];
+        $.each($('.companies'), function(i, val)
+        {
+            valoresCompany.push($(val).val().trim());
+        });
+        
+        //var sCompany=$('.company').val().trim();
+
+        var oJuego=  {nombre: sNombre,
+                        arrayCompany: JSON.stringify(valoresCompany),
+                        sinopsis: sSinopsis,
+                        enlace: sEnlace,
+                        fecha: sFecha};
+
+        var sDatos= "datos="+JSON.stringify(oJuego);
+        $.post("../servidor/gestionJuego/altaJuego.php",sDatos,function(bExito, sStatus, oAjax){
+        if(bExito==true)
+        {
+            $("#formAddJuego").hide();
+            $("h1").hide();
+            $("#guidelines").hide();
+            $("#registrado").show();
+            $("#registrarOtro").show();
+            /*
+            $.post("../servidor/gestionJuego/altaJuegosCompany.php",sDatos,function(bExito, sStatus, oAjax)
+            {
+
+            }
+            */
+            
+        }
+        else
+        {
+            $("#registroError").show();
+        }
+        },"json");
+        
+    }
+    else
+    {
+        $("#guidelines").show();
+    }
+}
+
+function respuestaAutoCompleteCompany(oRespuesta, sStatus, oAjax)
+{
+    if(oAjax.status==200)
+    {
+        
+
+        for(var i=0;i<oRespuesta.length;i++)
+        {
+            //arrayDNI.push(oRespuesta[i].dni);
+            var arrayDatos={};
+            arrayDatos["value"]=oRespuesta[i].nombre;
+            arrayDatos["desc"]="Fecha: "+oRespuesta[i].fecha+" - País: "+oRespuesta[i].pais;
+            ids.push(arrayDatos);
+        }
+        
+        $("#formAddJuego #company").autocomplete({
+            source: ids,
+            minLength: 0,
+            select: function(event, ui){
+                 $("#formAddJuego #company").val(ui.item.value);
+                 //$("#cliente-dni").val(ui.item.value);
+                 return false;
+            }}).autocomplete("instance")._renderItem=function(ul, item){
+                return $("<li>").append("<div>"+item.value+"<br>"+item.desc+"</div>").appendTo(ul);
+            };
+    }
+}
+
+function validarJuego()
+{
+    var res=true;
+
+    return res;
+}
---
+++ pages/addJuego.php
@@ -0,0 +1,66 @@
+﻿<?php
+include("../utilities/utilities.php");
+iniciarSesion();
+cabecera("Añadir Plataforma");
+navBar();
+?>
+
+<div id="registrado">
+    <h2>Añadido correctamente</h2>
+    <br>
+</div>
+<div id="registrarOtro">
+    <input type="button" id="btnAgain" class="btn btn-primary col-3" value="Registrar otro" />
+    <br>
+</div>
+
+<div id="registroError">
+    <h2 class="text-danger">Error al añadir</h2>
+    <br>
+</div>
+
+<h1>Añadir Juego </h1>
+<br>
+<div id="guidelines" class="col-6">
+<p> Si tiene alguna duda consulte la <a href="faq.php">FAQ</a></p>
+<p> Antes de añadir consulte si ya existe en la base de datos </p>
+</div>
+<br>
+<div id="registrar" class="row">
+    <div class="col-12 ">
+        <form name="formAddJuego" id="formAddJuego" method="get" action"#"> 
+        <div class="form-group">
+            <label for="nombre">Título: *</label>
+            <input type="text" class="form-control col-6" id="nombre" placeholder="Nombre completo" name="nombre">
+        </div>
+        <div class="form-group">
+            <label for="company">Compañía: *</label>
+            <input type="text" class="form-control col-6 companies" id="company" placeholder="Nombre completo" name="company">
+        </div>
+
+        <div class="text-center col-6">
+            <input type="button" id="btnCompany" class="btn btn-primary col-6" value="Añadir Otra Compañía" />
+            
+        </div>
+        <div class="form-group">
+            <label for="sinopsis">Sinopsis:</label>
+            <textarea class="form-control col-6" id="sinopsis" rows="5" placeholder="" name="sinopsis"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="enlace">Enlace de interés(Página oficial, wikipedia, etc):</label>
+            <input type="text" class="form-control col-6" id="enlace" placeholder="https://www.crashbandicoot.com/es" name="enlace">
+        </div>
+        <div class="form-group">
+            <label for="fecha">Fecha:</label>
+            <input type="text" class="form-control col-6" id="fecha" placeholder="yy-mm-dd" name="fecha">
+        </div>
+        <br>
+        <input type="button" id="btnADD" class="btn btn-primary col-6 mb-5" value="Añadir" />
+        </form>
+    </div>   
+</div>
+<script type="text/javascript" src="../js/addJuego.js"></script>
+<?php
+pie();
+?>
