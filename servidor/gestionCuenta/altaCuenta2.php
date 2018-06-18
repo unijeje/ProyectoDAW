08fb6721cf8e99046a51ce0daa93de1216fbbb7a
unijeje <veturn@hotmail.com>
2018-06-18 13:59:17 +0200

    correo de confirmacion al registrarse

--- servidor/gestionCuenta/altaCuenta2.php
+++ servidor/gestionCuenta/altaCuenta2.php
@@ -17,7 +17,22 @@ $stmt = DB::run($sql, [$usuario, $claveEncriptada, $correo, $date, $clave]);
 $n=$stmt->rowCount();
 
 if($n > 0)
+{
     $exito = true;
+    $headers[] = 'MIME-Version: 1.0';
+    $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+    
+    $message = "<html><head></head><body>";
+    $message .= "Buenas ".$usuario;
+    $message .= "<br><br>";
+    $message .= "Su registro en VJDB se ha realizado con exito. Puede conectarse con sus credenciales cuando lo desee.";
+    $message .= "<br>";
+    $message .= "Si no ha sido usted el que se ha registrado ignore este correo.";
+    $message .= "</body></html>";
+
+    mail($correo, "Bienvenido a VJDB", $message, implode("\r\n", $headers));
+}
+    
 else
     $exito = false;
 
